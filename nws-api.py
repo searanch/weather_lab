bfc97be338c96844bc3f15f19093e9a623d48ebf
lizbaconjones@gmail.com <lizbaconjones@gmail.com>
2020-09-27 14:18:51 -0500

    Imported noaa_sdk python package to more easily access data. Made api call and printed result.

--- nws-api.py
+++ nws-api.py
@@ -1,19 +1,19 @@
 import requests
 import json
 
-#Use get to make request for NWS api.
+# Impoted Python Package for a easier way to access api
 
-# Store url in variable
-url = "https://api.weather.gov/gridpoints/MPX/108,69/forecast"
+from noaa_sdk import noaa
 
-#response = requests.get(url)
+# Search fpr current weather in my zip code
+n = noaa.NOAA()
+res = n.get_forecasts('55407', 'US', True)
+for i in res:
+    print(i)
 
-#print(requests.get(url).json())
+print(type(i))
 
-# Pretty Print the output of the JSON
-response = requests.get(url).json()
-print(json.dumps(response, indent=4, sort_keys=True))
 
-# It is possible to grab a specific value 
-# from within the JSON object
-print(response["detailedForecast"])
+json_object = json.dumps(i, indent = 4)   
+print(json_object)  
+
